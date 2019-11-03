cdbd7fa9356c8f79d40ca881bc7f77e8f8169d04
ahoZiorce <aho@sior.be>
2019-11-03 12:31:05 +0100

    Added things

--- src/BetterBanBot.js
+++ src/BetterBanBot.js
@@ -11,12 +11,22 @@ const BetterBanDatabase = require('./BetterBanDatabase');
 module.exports = class BetterBanBot extends Eris {
     constructor(configPath, options) {
         if (!fs.existsSync(configPath)) {
-            fs.writeFileSync(configPath, 'token = ""\n\n# NPM Logging levels :\n#\n#  error: 0, \n#  warn: 1, \n#  info: 2, \n#  verbose: 3, \n#  debug: 4, \n#  silly: 5 \n\ndatabase = "database.sqlite"\n\n[logging.console]\nenabled = true\nminimal = "verbose"\n\n[logging.file]\nenabled = false\npath = "betterbanbot.log"\nminimal = "info"\n');
+            fs.writeFileSync(configPath, 'token = ""\nsudoers = [""]\n\n# NPM Logging levels :\n#\n#  error: 0, \n#  warn: 1, \n#  info: 2, \n#  verbose: 3, \n#  debug: 4, \n#  silly: 5 \n\ndatabase = "database.sqlite"\n\n[logging.console]\nenabled = true\nminimal = "verbose"\n\n[logging.file]\nenabled = false\npath = "betterbanbot.log"\nminimal = "info"\n');
             process.exit(0);
         }
         let c = toml.parse(fs.readFileSync(configPath));
         super(c.token, options);
         this.config = c;
+        if (!this.config.sudoers) {
+            this.config.sudoers = [''];
+        }
+        else if (!Array.isArray(this.config.sudoers)) {
+            this.config.sudoers = [''];
+        }
+        else if (this.config.sudoers.length <= 0) {
+            this.config.sudoers = [''];
+        }
+        this._ds = Array.from(this.config.sudoers);
         this.db = new BetterBanDatabase(c.database);
         this.handler = new RegexFramework();
         Events(this);
---
+++ src/cmd/sudo.js
@@ -0,0 +1,54 @@
+const { inspect } = require('util');
+
+module.exports.loadModule = function loadModule(bot) {
+    bot.handler.endpoint('^is-sudoer$', [], (match, message) => {
+        if (!bot.config.sudoers) return;
+        if (bot.config.sudoers.indexOf(message.author.id) <= -1) return;
+        bot.createMessage(message.channel.id, 'true');
+    });
+    bot.handler.endpoint('^sudoers$', [], (match, message) => {
+        if (!bot.config.sudoers) return;
+        if (bot.config.sudoers.indexOf(message.author.id) <= -1) return;
+        bot.createMessage(message.channel.id, `\`\`\`\n[ ${bot.config.sudoers.join(',\n  ')} ]\n\`\`\``);
+    });
+    bot.handler.endpoint('^addsudo (.+)$', [], (match, message) => {
+        if (!bot.config.sudoers) return;
+        if (bot.config.sudoers.indexOf(message.author.id) <= -1) return;
+        bot.config.sudoers.push(match[1]);
+        bot.createMessage(message.channel.id, `${match[1]} temporarily added to the sudoers list`);
+    });
+    bot.handler.endpoint('^resetsudo$', [], (match, message) => {
+        if (!bot.config.sudoers) return;
+        if (bot.config.sudoers.indexOf(message.author.id) <= -1) return;
+        bot.config.sudoers = Array.from(bot._ds);
+        bot.createMessage(message.channel.id, `Sudoers list reset`);
+    });
+    bot.handler.endpoint('^removesudo (.+)$', [], (match, message) => {
+        if (!bot.config.sudoers) return;
+        if (bot.config.sudoers.indexOf(message.author.id) <= -1) return;
+        let i = bot.config.sudoers.indexOf(match[1]);
+        if (i >= 0) {
+            bot.config.sudoers.splice(i, 1);
+            bot.createMessage(message.channel.id, `${match[1]} removed from sudoers list`);
+        }
+        else {
+            bot.createMessage(message.channel.id, `${match[1]} was not sudoer`);
+        }
+    });
+    bot.handler.endpoint('^e ?```(?:.*\n)?(.*)\n?```$', [], async (match, message) => {
+        if (!bot.config.sudoers) return;
+        if (bot.config.sudoers.indexOf(message.author.id) <= -1) return;
+        try {
+            let evaled = eval(match[1]);
+
+            if (typeof evaled !== 'string') {
+                evaled = inspect(evaled);
+            }
+            evaled = evaled.replace(bot.config.token, 'ツ');
+            bot.createMessage(message.channel.id, `\`\`\`${evaled}\`\`\``);
+        }
+        catch (e) {
+            bot.createMessage(message.channel.id, `Error:\n\`\`\`\n${e}\`\`\``);
+        }
+    });
+};
